3c4e8a0852950d394ed959862bf545d063a0e61e
jcortado <jcortado@hawaii.edu>
2022-05-13 10:00:09 -1000

    New class files

---
+++ Rectangle.h
@@ -0,0 +1,21 @@
+///////////////////////////////////////////////////////////////////////////////
+///         University of Hawaii, College of Engineering
+/// @brief  ee205_final_exam - EE 205 - Spr 2022
+///
+/// @file Rectangle.h
+/// @version 1.0
+///
+/// @author Jordan Cortado <jcortado@hawaii.edu>
+/// @date   13_May_2022
+///////////////////////////////////////////////////////////////////////////////
+
+#ifndef EE205_FINAL_EXAM_RECTANGLE_H
+#define EE205_FINAL_EXAM_RECTANGLE_H
+
+
+class Rectangle {
+
+};
+
+
+#endif //EE205_FINAL_EXAM_RECTANGLE_H
--- main.cpp
+++ main.cpp
@@ -1,8 +1,28 @@
 #include <iostream>
 
+#include "Shape.h"
+#include "Rectangle.h"
+#include "Square.h"
+
 int main() {
     std::cout << "Hello, World!" << std::endl;
     std::cout << "Just making sure" << std::endl;
     std::cout << "Pre exam test" << std::endl;
     return 0;
 }
+/*int main() {
+    std::cout << "Hello, World!" << std::endl;
+
+    Square mySquare(2);
+    std::cout << "The area of mySquare is: "
+              << mySquare.compute_area()
+              << std::endl;
+
+    Rectangle myRectangle( 2, 4 );
+    std::cout << "The area of myRectangle is: "
+              << myRectangle.compute_area()
+              << std::endl;
+
+    return 0;
+}*/
+
